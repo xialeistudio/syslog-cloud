a3e07ab1bb62bda98b161b3cb9e37781c677a260
xialeistudio <xialeistudio@gmail.com>
2017-10-18 16:11:37 +0800

    change logger level

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ class Application {
   }
 
   private startup() {
-    this.syslogDaemon.listen(this.logPort, () => console.log('123'));
-    logger.info(`syslogd startup on ${this.logPort}`);
+    this.syslogDaemon.listen(this.logPort);
+    logger.info(`syslogd startup on udp:${this.logPort}`);
   }
 }
 
