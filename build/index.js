8589e65983b00319f47da22a71b048494c0c89bd
xialeistudio <xialeistudio@gmail.com>
2017-10-18 16:09:26 +0800

    add bootstrap

--- build/index.js
+++ build/index.js
@@ -4,12 +4,22 @@ const moment = require("moment");
 const syslogd = require("syslogd");
 const logger_1 = require("./common/logger");
 const syslog_1 = require("./models/syslog");
-(async () => {
-    // syslogd
-    const logPort = Number(process.env.LOG_PORT || 514);
-    syslogd(async (data) => {
+class Application {
+    constructor() {
+        this.logPort = Number(process.env.LOG_PORT || 514);
+        this.syslogDaemon = syslogd(data => this.handleLogData(data));
+        this.startup();
+    }
+    static bootstrap() {
+        return new Application();
+    }
+    async handleLogData(data) {
         data.time = moment(data.time).toDate();
         await syslog_1.default.create(data);
-        logger_1.default.info(data.address, data.msg);
-    }).listen(logPort, () => logger_1.default.info(`syslogd startup on ${logPort}`));
-})();
+    }
+    startup() {
+        this.syslogDaemon.listen(this.logPort);
+        logger_1.default.info(`syslogd startup on ${this.logPort}`);
+    }
+}
+Application.bootstrap();
